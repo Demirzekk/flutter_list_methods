0924073e503bc48b313f20e22fd98e5bccbe817e
turanulupinar <turanulup@gmail.com>
2023-11-09 17:22:30 +0300

    ui çizildi

--- lib/apple_news/apple_news_ui.dart
+++ lib/apple_news/apple_news_ui.dart
@@ -26,16 +26,23 @@ class _AppleNewsUIState extends State<AppleNewsUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(appleModel?.bigLogo ?? ""),
-                      fit: BoxFit.cover))),
+          Padding(
+            padding: const EdgeInsets.only(right: 190, bottom: 5),
+            child: Container(
+                height: 110,
+                width: 110,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: NetworkImage(
+                    appleModel?.bigLogo ?? "",
+                  ),
+                  fit: BoxFit.cover,
+                ))),
+          ),
           Center(
               child: Text(
             appleModel?.title1 ?? "",
@@ -57,7 +64,13 @@ class _AppleNewsUIState extends State<AppleNewsUI> {
               right: 90,
               left: 90,
             ),
-            child: Text(appleModel?.subtitle ?? ""),
+            child: Text(
+              appleModel?.subtitle ?? "",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.copyWith(color: Colors.black),
+            ),
           ),
           const SizedBox(
             height: 150,
@@ -68,42 +81,54 @@ class _AppleNewsUIState extends State<AppleNewsUI> {
             color: Colors.pink,
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 80,
+            ),
             child: RichText(
+                textAlign: TextAlign.center,
                 text: TextSpan(
                     text: appleModel?.desc ?? "null",
-                    style: const TextStyle(color: Colors.black),
+                    style: Theme.of(context).textTheme.bodySmall,
                     children: [
-                  TextSpan(
-                      text: appleModel?.seeLink?.linkText ?? "",
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          log("Text'e tıklandı: ${appleModel?.seeLink?.linkUrl}");
-                          launchUrl(
-                              Uri.parse(appleModel?.seeLink?.linkUrl ??
-                                  "https://www.google.com.tr"),
-                              mode: LaunchMode.externalApplication);
-                        },
-                      style: TextStyle(
-                          color: Color(appleModel?.seeLink?.linkColor ?? 0)))
-                ])),
+                      TextSpan(
+                          text: appleModel?.seeLink?.linkText ?? "",
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              log("Text'e tıklandı: ${appleModel?.seeLink?.linkUrl}");
+                              launchUrl(
+                                  Uri.parse(appleModel?.seeLink?.linkUrl ??
+                                      "https://www.google.com.tr"),
+                                  mode: LaunchMode.externalApplication);
+                            },
+                          style: TextStyle(
+                              color:
+                                  Color(appleModel?.seeLink?.linkColor ?? 0)))
+                    ])),
           ),
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(appleModel?.button?.buttonColor ?? 0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12))),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            appleModel?.button?.buttonRoute ??
-                            const SizedBox.shrink()));
-              },
-              child: Text(
-                appleModel?.button?.buttonText ?? "",
-              ))
+          Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: Container(
+              width: 250,
+              height: 50,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Color(appleModel?.button?.buttonColor ?? 0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                appleModel?.button?.buttonRoute ??
+                                const SizedBox.shrink()));
+                  },
+                  child: Text(
+                    appleModel?.button?.buttonText ?? "",
+                  )),
+            ),
+          )
         ],
       ),
     );
